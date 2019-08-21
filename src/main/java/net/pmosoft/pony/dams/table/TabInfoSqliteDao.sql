2d49681c183b3d329c1e935f320367eee83536f9
P147970 <P147970@SKTP147970PN001.SKT.AD>
2019-08-21 18:26:43 +0900

    ..

--- src/main/java/net/pmosoft/pony/dams/table/TabInfoSqliteDao.sql
+++ src/main/java/net/pmosoft/pony/dams/table/TabInfoSqliteDao.sql
@@ -1,4 +1,26 @@
-
+    
+    SELECT DISTINCT
+           A.JDBC_NM
+         , A.OWNER
+         , A.TAB_NM
+         , A.TAB_HNM
+         , A.TAB_ROWS
+         , A.TAB_REG_DT
+         , A.TAB_REG_DT2
+         , A.TAB_UPD_DT
+         , A.TAB_UPD_DT2    
+FROM   TDACM00080 A 
+    WHERE  UPPER(A.JDBC_NM) LIKE ('%'||UPPER(''  )||'%')
+    AND    UPPER(A.OWNER  ) LIKE ('%'||UPPER(''   )||'%')
+    AND    UPPER(A.TAB_NM ) LIKE ('%'||UPPER(''   )||'%')
+    AND    UPPER(IFNULL(A.TAB_HNM,'')) LIKE ('%'||UPPER(''  )||'%')
+    AND    UPPER(A.JDBC_NM) IN (SELECT DISTINCT UPPER(JDBC_NM) FROM TDACM00010)     
+    AND    A.TAB_ROWS       = A.TAB_ROWS 
+    AND    A.TAB_REG_DT     >= (CASE WHEN '' = '' THEN '19010101' ELSE '' END)
+    AND    A.TAB_UPD_DT     >= (CASE WHEN '' = '' THEN '19010101' ELSE '' END)
+    ORDER BY 1 ASC,A.JDBC_NM,A.OWNER,A.TAB_NM
+    
+    
 
 WITH TEMP01 AS (
 SELECT OWNER

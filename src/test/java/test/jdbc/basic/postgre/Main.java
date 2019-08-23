68a924c6cedfba6c629faefef8fee542233e054e
P147970 <P147970@SKTP147970PN001.SKT.AD>
2019-08-23 18:19:30 +0900

    ..

---
+++ src/test/java/test/jdbc/basic/postgre/Main.java
@@ -0,0 +1,54 @@
+package test.jdbc.basic.postgre;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	TestJdbcBasicSqlite sqliteBasicJdbc = new TestJdbcBasicSqlite();
+    	sqliteBasicJdbc.test01();
+    }
+}
+
+class TestJdbcBasicSqlite {
+
+    public Connection conn = null;
+    public Statement stmt = null;
+    public ResultSet rs = null;
+
+    TestJdbcBasicSqlite() { DBConn(); }
+
+    public void test01(){
+
+        try {
+            stmt = conn.createStatement();
+            String query = "select * from (select '1' empno, 'abc' ename union all select '2' empno, 'def' ename) emp";
+            rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String empno = rs.getString("empno");
+                String ename = rs.getString("ename");
+                System.out.println("empno="+empno+" ename=" + ename);
+            }
+        } catch ( Exception e ) { e.printStackTrace(); } finally { DBClose(); }
+
+    }
+
+    void DBConn(){
+
+        String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+        String DB_USER = "postgres";
+        String DB_PASSWORD = "1";
+
+        try {
+        	Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch ( Exception e ) { e.printStackTrace(); } finally {}
+    }
+
+    void DBClose(){ rs = null; stmt = null; conn = null; }
+}
+

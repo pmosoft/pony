3c66cf7e42c3f1b7061c7d5d9bdf048da6b28e10
P147970 <P147970@SKTP147970PN001.SKT.AD>
2019-08-29 18:21:00 +0900

    ..

--- doc/02.DB/q01.sql
+++
@@ -1,8 +0,0 @@
-select * from 족보;
-
-drop table 족보
-
-INSERT INTO 족보 VALUES ('180101', '우세', '우세', '170101', '', '', '', '남', '', '', 'Y', '우世', '', '');
-
-
-ALTER TABLE 족보 convert to charset utf8;

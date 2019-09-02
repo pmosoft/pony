b7c48999ddb09c1aab362c72c42a14fa38db08f8
P147970 <P147970@SKTP147970PN001.SKT.AD>
2019-09-02 18:29:02 +0900

    ..

--- src/main/java/net/pmosoft/pony/comm/excel/ExcelSrv.java
+++ src/main/java/net/pmosoft/pony/comm/excel/ExcelSrv.java
@@ -36,8 +36,7 @@ public class ExcelSrv {
 
 
         String data = params.get("data");
-        System.out.println("data="+data);
-        System.out.println("data="+data);
+        System.out.println("data11111="+data);
         Gson gson = new Gson();
         Type type = new TypeToken<List<Map<String,String>>>() {}.getType();
         List<Map<String,String>> listParams  = gson.fromJson(data, type);

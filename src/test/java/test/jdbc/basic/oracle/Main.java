531a0b8c8839a9706527a09847276b690bf1c4c4
Administrator <Administrator@DESKTOP-6V7L04O>
2019-09-27 10:43:00 +0900

    ..

--- src/test/java/test/jdbc/basic/oracle/Main.java
+++ src/test/java/test/jdbc/basic/oracle/Main.java
@@ -45,6 +45,8 @@ class TestJdbcBasicOracle {
         //String DB_URL = "jdbc:oracle:thin:@localhost:1521/ORCL"; String DB_USER = "cellplan"; String DB_PASSWORD = "cell_2012";
         //String DB_URL = "jdbc:oracle:thin:@pmosoft.iptime.org:11521/ORCL"; String DB_USER = "scott"; String DB_PASSWORD = "1";
         String DB_URL = "jdbc:oracle:thin:@pmosoft.iptime.org:11521/ORCL"; String DB_USER = "cellplan"; String DB_PASSWORD = "cell_2012";
+        //String DB_URL = "jdbc:oracle:thin:@localhost:1521/ORCL"; String DB_USER = "scott"; String DB_PASSWORD = "1";
+        //String DB_URL = "jdbc:oracle:thin:@192.168.0.6:1521/ORCL"; String DB_USER = "scott"; String DB_PASSWORD = "1";
         //String DB_URL = "jdbc:oracle:thin:@localhost:9951/IAMLTE"; String DB_USER = "cellplan"; String DB_PASSWORD = "cell_2012";
 
         try {
@@ -55,4 +57,7 @@ class TestJdbcBasicOracle {
 
     void DBClose(){ rs = null; stmt = null; conn = null; }
 }
+
+
+
 

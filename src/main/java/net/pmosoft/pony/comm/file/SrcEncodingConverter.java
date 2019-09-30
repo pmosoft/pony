0e9087c02b3919e6549332b4a9c94bcdc211aa58
Administrator <Administrator@DESKTOP-6V7L04O>
2019-09-30 18:31:44 +0900

    ..

--- src/main/java/net/pmosoft/pony/comm/file/SrcEncodingConverter.java
+++ src/main/java/net/pmosoft/pony/comm/file/SrcEncodingConverter.java
@@ -1,6 +1,5 @@
 package net.pmosoft.pony.comm.file;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,21 +15,21 @@ import java.util.regex.Pattern;
 
 /**
  * Java 소스의 인코딩을 일괄 변경한다
- * 
+ *
  * 사용법
  * args[0] : 소스 input 폴더
  * args[1] : 변경소스 output 폴더
  * args[2] : 파일패턴 정규식
  * args[3] : 현재 Encoding
  * args[4] : 바꿀 Encoding
- * 
+ *
  * Ex) java -jar kr.innofactory.util.SrcEncodingConverter C:\work\workspace\Myproject\src c:\temp\src .*\.java MS949 8859_1
- * 
+ *
  * @author gryu@innofactory.kr
  *
  */
 public class SrcEncodingConverter {
-    
+
     /** 입력 디렉토리 */
     File inputDir;
     /** 출력 디렉토리 */
@@ -41,7 +40,34 @@ public class SrcEncodingConverter {
     String oldEncoding;
     /** 신규 인코딩 */
     String newEncoding;
-    
+
+    /**
+     * 메인 메쏘드
+     * @param args
+     */
+    public static void main(String[] args) {
+        String inputDirPath = args[0];
+        String outputDirPath = args[1];
+        String filePattern = args[2];
+        String oldEncoding = args[3];
+        if (oldEncoding.equalsIgnoreCase("null")) {
+            oldEncoding = null;
+        }
+        String newEncoding = args[4];
+        if (newEncoding.equalsIgnoreCase("null")) {
+            newEncoding = null;
+        }
+
+
+        SrcEncodingConverter converter = new SrcEncodingConverter(
+                inputDirPath, outputDirPath, filePattern, oldEncoding, newEncoding);
+        converter.execute();
+    }
+
+
+
+
+
     /**
      * 생성자
      * @param inputDirPath
@@ -50,7 +76,7 @@ public class SrcEncodingConverter {
      * @param oldEncoding
      * @param newEncoding
      */
-    public SrcEncodingConverter(String inputDirPath, 
+    public SrcEncodingConverter(String inputDirPath,
             String outputDirPath, String filePattern, String oldEncoding, String newEncoding) {
         this.inputDir = new File(inputDirPath);
         this.outputDir = new File(outputDirPath);
@@ -58,14 +84,14 @@ public class SrcEncodingConverter {
         this.oldEncoding = oldEncoding;
         this.newEncoding = newEncoding;
     }
-    
+
     /**
      * 변환 실행
      */
     public void execute() {
         // #. 재귀적으로 대상 파일 가져오기
         List<File> inputFiles = getAllFiles(inputDir);
-        
+
         for (int i = 0; i < inputFiles.size(); i++) {
             File inputFile = inputFiles.get(i);
             File outputFile = getOuputFile(inputFile);
@@ -77,14 +103,14 @@ public class SrcEncodingConverter {
             }
         }
     }
-    
+
     /**
      * 전체 파일목록을 얻는다
      * @return
      */
     private List<File> getAllFiles(File dir) {
         ArrayList<File> allFiles = new ArrayList<File>();
-        
+
         File[] files = dir.listFiles();
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
@@ -100,9 +126,9 @@ public class SrcEncodingConverter {
         }
         return allFiles;
     }
-    
+
     /**
-     * outputDir 하위에 저장될 outputFile 객체를 얻는다 
+     * outputDir 하위에 저장될 outputFile 객체를 얻는다
      * @param inputFile
      * @return
      */
@@ -110,13 +136,13 @@ public class SrcEncodingConverter {
         String outputDirPath = this.outputDir.getAbsolutePath();
         String inputDirPath = this.inputDir.getAbsolutePath();
         String inputFilePath = inputFile.getAbsolutePath();
-        
+
         String relativePath = inputFilePath.substring(inputDirPath.length());
         String outputFilePath = outputDirPath + relativePath;
-        
+
         return new File(outputFilePath);
     }
-    
+
     /**
      * 파일 변환을 실행한다
      * @param inputFile
@@ -125,17 +151,17 @@ public class SrcEncodingConverter {
      * @throws IOException
      */
     private File convertSrcFile(File inputFile, File outputFile) throws IOException {
-        
+
         // #. output 디렉토리 존재여부
         File parentDir = outputFile.getParentFile();
         if (!parentDir.exists()) {
             parentDir.mkdirs();
         }
-        
+
         // #. 파일 변환
         BufferedReader reader = null;
         PrintWriter writer = null;
-        
+
         try {
             if (this.oldEncoding != null) {
                 reader = new BufferedReader(
@@ -151,7 +177,7 @@ public class SrcEncodingConverter {
                 writer = new PrintWriter(
                         new OutputStreamWriter(new FileOutputStream(outputFile)));
             }
-            
+
             String line;
             while ((line = reader.readLine()) != null) {
                 writer.println(line);
@@ -160,30 +186,9 @@ public class SrcEncodingConverter {
             if (reader != null) reader.close();
             if (writer != null) writer.close();
         }
-        
+
         return outputFile;
     }
-    
-    /**
-     * 메인 메쏘드
-     * @param args
-     */
-    public static void main(String[] args) {
-        String inputDirPath = args[0]; 
-        String outputDirPath = args[1];
-        String filePattern = args[2];
-        String oldEncoding = args[3];
-        if (oldEncoding.equalsIgnoreCase("null")) {
-            oldEncoding = null;
-        }
-        String newEncoding = args[4];
-        if (newEncoding.equalsIgnoreCase("null")) {
-            newEncoding = null;
-        }
-        
-        SrcEncodingConverter converter = new SrcEncodingConverter(
-                inputDirPath, outputDirPath, filePattern, oldEncoding, newEncoding);
-        converter.execute();
-    }
+
 
 }
